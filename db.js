80c4472cbda91df8f0adbbf95b8cd0f351a6b8c9
guptadeepak5247 <guptadeepak5247@gmail.com>
2026-07-19 00:09:51 +0530

    env file addded

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 // here we will stablish a connection with mongodb
 const mongoose=require( "mongoose");
-const mongoURL="mongodb://127.0.0.1:27017/hotels";// this is the url of our database hotel
+require('dotenv').config();
+const mongoURL=process.env.MONGO_URL
+//const mongoURL="mongodb://127.0.0.1:27017/hotels";// this is the url of our database hotel
+//const mongoURL="mongodb+srv://deepak-123:<Deegu12#>@cluster0.jbg4qwu.mongodb.net/?appName=Cluster0";
 // we will use this function to connect to the database
 mongoose.connect(mongoURL);                              
 // mongoose maintain a default connection object representing the mongoDB connection
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const db=require('./db.js');
 const app=express();
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());         //req.body daal dega data ko jo bhi json format me hoga
-app.listen(3000, () => {
+require('dotenv').config();
+const port=process.env.PORT;
+app.listen(port, () => {
   console.log('Server is running on http://localhost:3000');   // for checking the server is running or not we can use this
 })
 const routerperson=require('./router/routerperson.js');
